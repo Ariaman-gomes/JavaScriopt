dc99ffe9e8a8166822e54a216d50b74ee68800e0
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-08 20:14:51 -0300

    atualizado

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,4 +1,4 @@
-var idade = 90
+var idade = 50
 console.log(`você tes ${idade} anos`)
 if (idade < 16) {
     console.log('Não vota')
--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,5 +1,32 @@
-var agora = new Data()
-var dieSem = agora.getDay()
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
 
 
-console.log(dieSem)
+switch(diaSem)
+{
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+            console.log('Segunda')
+            break
+    case 3:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+        default:
+        console.log('[ERRO]Dia inválido!')
+}
