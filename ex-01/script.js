96cb418bb36a0e449134ceb859ac0b047d911817
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-10 07:31:09 -0300

    atualizado

--- ex-01/script.js
+++ ex-01/script.js
@@ -1,1 +1,19 @@
-alert('Olá!, seja bem vindo ao nosso site...')
+function carregar(){
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && hora < 12){
+    //BOM DIA!
+    img.src = 'Fotomanha'
+  }
+  else if(hora >= 12 && hora <18){
+    //BOA TARDE
+    img.src = 'Fototarde'
+  }
+  else {
+    //BOA NOITE
+    img.src = 'Fotonoite'
+  }
+}
