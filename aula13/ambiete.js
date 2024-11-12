a8c90d905d6bf3af2a704efa84967b6a1dca74b6
Ariaman-gomes <148455368+Ariaman-gomes@users.noreply.github.com>
2024-11-12 06:46:04 -0300

    atualizado

---
+++ aula13/ambiete.js
@@ -0,0 +1,16 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <=15)
+
+
+/*var c = 1
+while (c <= 10) {
+    console.log('Tudo bem')
+    c++
+}
+*/
+
+/*console.log('Tudo bem?')*/
+
